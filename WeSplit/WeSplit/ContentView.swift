c19c837418e69db8df98f5f0652f11a53ca3ba1e
RachelRWang <rw533@cornell.edu>
2024-03-12 01:15:32 -0700

    Day 18
    project 1 done

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -18,6 +18,27 @@ struct ContentView: View {
     // we bind the text field so that it shows the value of our property, but we also bind it so that any changes to the text field also update the property.
     @State private var name = ""
     
+//    One of the great things about the @State property wrapper is that it automatically watches for changes, and when something happens it will automatically re-invoke the body property. That’s a fancy way of saying it will reload your UI to reflect the changed state, and it’s a fundamental feature of the way SwiftUI works.
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    // This is exactly like a regular @State property, except it’s specifically designed to handle input focus in our UI.
+    @FocusState private var amountIsFocused: Bool
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    // computed var will be recalcuated when swift UI state changes.
+    var totalPerPerson: Double {
+        // calculate the total per perosn here
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
     let students = ["Harry", "Herminone", "Ron"]
     @State private var selectedStudent = "Harry"
     var body: some View {
@@ -59,8 +80,39 @@ struct ContentView: View {
                         Text("Row \($0)")
                     }
                 }
+                
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD")).focused($amountIsFocused)
+                    Picker("Number of people", selection: $numberOfPeople){
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                }
+                
+                Section ("How much do you want to tip?"){
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self)
+                        {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
             }
-            .navigationTitle("SwiftUI")
+            .toolbar{
+                if amountIsFocused {
+                    Button("Done"){
+                        amountIsFocused = false
+                    }
+                }
+            }
+            .navigationTitle("We Split")
             .navigationBarTitleDisplayMode(.inline)
         }
     }
